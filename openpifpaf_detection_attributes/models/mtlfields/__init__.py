fb539a2a940f0495315c0509767ede2d1df46c4a
Taylor Mordan <taylor.mordan@epfl.ch>
2021-04-26 19:22:02 +0200

    add backbones with fork-normalization

--- openpifpaf_detection_attributes/models/mtlfields/__init__.py
+++ openpifpaf_detection_attributes/models/mtlfields/__init__.py
@@ -9,8 +9,14 @@ from ...datasets import headmeta
 
 def register():
     openpifpaf.BASE_TYPES.add(ForkNormNetwork)
+    openpifpaf.BASE_FACTORIES['fn-mobilenetv2'] = lambda: ForkNormNetwork(
+        'fn-mobilenetv2', 'mobilenetv2')
     openpifpaf.BASE_FACTORIES['fn-resnet50'] = lambda: ForkNormNetwork(
         'fn-resnet50', 'resnet50')
+    openpifpaf.BASE_FACTORIES['fn-shufflenetv2k16'] = lambda: ForkNormNetwork(
+        'fn-shufflenetv2k16', 'shufflenetv2k16')
+    openpifpaf.BASE_FACTORIES['fn-shufflenetv2k30'] = lambda: ForkNormNetwork(
+        'fn-shufflenetv2k30', 'shufflenetv2k30')
     openpifpaf.HEADS[headmeta.AttributeMeta] = AttributeField
     openpifpaf.DECODERS.add(InstanceDecoder)
     openpifpaf.LOSSES[headmeta.AttributeMeta] = AttributeLoss
